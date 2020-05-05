004226aac6b94a468976c70e47676a41afa4ce82
Patrick <yjs990427@gmail.com>
2020-05-05 20:06:29 +0900

    Ch. 7 TImer

--- boot/Main.c
+++ boot/Main.c
@@ -1,11 +1,16 @@
 #include "stdint.h"
 #include "HalInterrupt.h"
 #include "HalUart.h"
+#include "HalTimer.h"
 
 #include "stdio.h"
+#include "stdbool.h"
+#include "stdlib.h"
 
 static void Hw_init(void);
+
 static void Printf_test(void);
+static void Timer_test(void);
 
 void main(void) 
 {
@@ -22,12 +27,15 @@ void main(void)
 
     Printf_test();
 
+    Timer_test();
+
     while(true);
 }
 
 static void Hw_init(void) 
 {
     Hal_interrupt_init();
+    Hal_timer_init();
     Hal_uart_init();
 }
 
@@ -36,6 +44,7 @@ static void Printf_test(void)
     char* str = "printf pointer test";
     char* nullptr = 0;
     uint32_t i = 5;
+    uint32_t* sysctrl0 = (uint32_t*)0x10001000;
 
     debug_printf("%s\n", "Hello printf");
     debug_printf("output string pointer: %s\n", str);
@@ -43,4 +52,14 @@ static void Printf_test(void)
     debug_printf("%u = 5\n", i);
     debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
     debug_printf("print zero %u\n", 0);
+    debug_printf("SYSCTRL0 %x\n", *sysctrl0);
+}
+
+static void Timer_test(void)
+{
+    while(true)
+    {
+        debug_printf("current count : %u\n", Hal_timer_get_1ms_counter());
+        delay(1000);
+    }
 }
--- hal/rvpb/Regs.c
+++ hal/rvpb/Regs.c
@@ -1,7 +1,9 @@
 #include "stdint.h"
 #include "Uart.h"
+#include "Timer.h"
 #include "Interrupt.h"
 
 volatile PL011_t*   Uart    = (PL011_t*)UART_BASE_ADDRESS0;
+volatile Timer_t*   Timer   = (Timer_t*)TIMER_CPU_BASE;
 volatile GicCput_t* GicCpu  = (GicCput_t*)GIC_CPU_BASE;
 volatile GicDist_t* GicDist = (GicDist_t*)GIC_DIST_BASE;
